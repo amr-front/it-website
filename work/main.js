e5b54a0b1ec37cfa68646c3bb114e8c13b6e2270
amr-front <amrfront01f@gmail.com>
2024-11-23 20:53:36 +0200

    Add files via upload

---
+++ work/main.js
@@ -0,0 +1,18 @@
+const bar = document.querySelector(".bars");
+const links = document.querySelector(".link-holder");
+
+bar.addEventListener("click", () => {
+  links.classList.toggle("view");
+  if (links.classList.contains("view")) {
+    bar.children[0].style.transform = "rotate(45deg)";
+  } else {
+    bar.children[0].style.transform = "rotate(0deg)";
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (!bar.contains(e.target) && !links.contains(e.target)) {
+    links.classList.remove("view");
+    bar.children[0].style.transform = "rotate(0deg)";
+  }
+});
